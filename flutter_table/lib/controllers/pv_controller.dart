9f84167db8742cd0bd6f4617f255643c8aff749d
raihansikdar <raihansikdar10@gmail.com>
2024-03-13 12:33:15 +0600

    try to add pagination

--- flutter_table/lib/controllers/pv_controller.dart
+++ flutter_table/lib/controllers/pv_controller.dart
@@ -8,17 +8,17 @@ import 'package:flutter_table/utility/app_urls.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart';
 
-class PVController extends GetxController{
+class PVController extends GetxController {
   bool isLoading = false;
-
+  int currentPage = 0;
 
   @override
-  void onInit(){
+  void onInit() {
     getInv1Data();
     super.onInit();
   }
 
-  List<PVModel> invList =[];
+  List<PVModel> invList = [];
   Future<void> getInv1Data() async {
     isLoading = true;
     update();
@@ -33,28 +33,50 @@ class PVController extends GetxController{
     log('====================');
 
     try {
+      if (response1.isSuccess && response2.isSuccess && response3.isSuccess) {
+        PVModel pvModelInv1 = PVModel.fromJson(response1.body ?? {});
+        PVModel pvModelInv2 = PVModel.fromJson(response2.body ?? {});
+        PVModel pvModelInv3 = PVModel.fromJson(response3.body ?? {});
 
-     if(response1.isSuccess && response2.isSuccess && response3.isSuccess){
-       PVModel pvModelInv1 = PVModel.fromJson(response1.body ?? {});
-       PVModel pvModelInv2 = PVModel.fromJson(response2.body ?? {});
-       PVModel pvModelInv3 = PVModel.fromJson(response3.body ?? {});
+        invList.add(pvModelInv1);
+        invList.add(pvModelInv2);
+        invList.add(pvModelInv3);
 
-       invList.add(pvModelInv1);
-       invList.add(pvModelInv2);
-       invList.add(pvModelInv3);
-
-       isLoading = false;
-       update();
-     }else{
-       isLoading = false;
-       update();
-
-     }
-
+        isLoading = false;
+        update();
+      } else {
+        isLoading = false;
+        update();
+      }
     } catch (e) {
       log("Exception: $e");
     }
   }
 
-
+  void previousPage() {
+    if (currentPage > 0) {
+      currentPage--;
+      update();
     }
+  }
+
+  void nextPage() {
+    final int totalPages = (invList.length / 10).ceil(); // Calculate total pages
+    if (currentPage < totalPages - 1) {
+      currentPage++;
+      update();
+    }
+  }
+
+
+  List<PVModel> getPaginatedData(int currentPage, int itemsPerPage) {
+    final int startIndex = currentPage * itemsPerPage;
+    final int endIndex = (currentPage + 1) * itemsPerPage;
+
+    return invList.sublist(
+      startIndex,
+      endIndex < invList.length ? endIndex : invList.length,
+    );
+  }
+}
+
--- flutter_table/lib/views/home_screen.dart
+++ flutter_table/lib/views/home_screen.dart
@@ -1,108 +1,8 @@
-/*
-import 'package:flutter/material.dart';
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-       body: Padding(
-         padding: const EdgeInsets.all(8.0),
-         child: DataTable(
-             columns: const[
-              DataColumn(label: Text('Name')),
-              DataColumn(label: Text('Inv1')),
-              DataColumn(label: Text('Inv2')),
-              DataColumn(label: Text('Inv3')),
-         ], rows: const [
-           DataRow(cells: [
-             DataCell(Text('PV01Voltage')),
-             DataCell(Text('1.0')),
-             DataCell(Text('2.0')),
-             DataCell(Text('3.0')),
-           ]),
-           DataRow(cells: [
-             DataCell(Text('PV01Current')),
-             DataCell(Text('1.0')),
-             DataCell(Text('2.0')),
-             DataCell(Text('3.0')),
-           ]),
-           DataRow(cells: [
-             DataCell(Text('PV02Voltage')),
-             DataCell(Text('1.0')),
-             DataCell(Text('2.0')),
-             DataCell(Text('3.0')),
-           ]),
-           DataRow(cells: [
-             DataCell(Text('PV02Current')),
-             DataCell(Text('1.0')),
-             DataCell(Text('2.0')),
-             DataCell(Text('3.0')),
-           ]),
-         ]),
-       ),
-    ));
-  }
-}
-*/
-import 'package:flutter/material.dart';
-import 'package:flutter_table/controllers/pv_controller.dart';
-import 'package:get/get.dart';
-
-/*class HomeScreen extends StatelessWidget {
-   HomeScreen({Key? key});
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GetBuilder<PVController>(
-            builder: (_pvController) {
-             print(  _pvController.myList.length);
-              if(_pvController.isLoading){
-                return const Center(child: CircularProgressIndicator());
-              }
-              return FittedBox(
-                child: DataTable(
-                  columns: const [
-                    DataColumn(label: Text('Name')),
-                    DataColumn(label: Text('Inv1')),
-                    DataColumn(label: Text('Inv2')),
-                    DataColumn(label: Text('Inv3')),
-                  ],
-                  rows: List<DataRow>.generate(
-                    _pvController.myList.length,
-                        (index) {
-                      final inv1ListData = _pvController.myList[index];
-                      return DataRow(
-                        cells: [
-                          DataCell(Text('pv01Voltage')),
-                          DataCell(Text(inv1ListData.pv01Voltage.toString())),
-                          DataCell(Text(inv1ListData.pv02Voltage.toString())),
-                          DataCell(Text(inv1ListData.pv03Voltage.toString())),
-                        ],
-                      );
-                    },
-                  ),
-                ),
-              );
-            }
-          ),
-        ),
-      ),
-    );
-  }
-}*/
 import 'package:flutter/material.dart';
 import 'package:flutter_table/controllers/pv_controller.dart';
 import 'package:flutter_table/pv_model/pv_model.dart';
 import 'package:get/get.dart';
+
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key});
@@ -110,43 +10,78 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GetBuilder<PVController>(
-                builder: (_pvController) {
-                  print(_pvController.invList.length);
-                  if (_pvController.isLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  return DataTable(
-                    columns: const [
-                      DataColumn(label: Text('Attribute')),
-                      DataColumn(label: Text('Value (Inverter 1)')),
-                      DataColumn(label: Text('Value (Inverter 2)')),
-                      DataColumn(label: Text('Value (Inverter 3)')),
-                    ],
-                    rows: [
-                      ..._pvController.invList[0].toJson().entries.map((entry) {
-                        final attribute = entry.key;
-                        final inv1Value = entry.value.toString();
-                        final inv2Value = _pvController.invList[1].toJson()[entry.key].toString();
-                        final inv3Value = _pvController.invList[2].toJson()[entry.key].toString();
-                        return DataRow(
-                          cells: [
-                            DataCell(Text(attribute)),
-                            DataCell(Text(inv1Value)),
-                            DataCell(Text(inv2Value)),
-                            DataCell(Text(inv3Value)),
-                          ],
-                        );
-                      }),
-                    ],
-                  );
-                },
-              ),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GetBuilder<PVController>(
+              builder: (_pvController) {
+                if(_pvController.isLoading){
+                  return Center(child: CircularProgressIndicator());
+                }
+                const int itemsPerPage = 10;
+                final int currentPage = _pvController.currentPage;
+                final List<PVModel> invList = _pvController.invList;
+
+                final int startIndex = currentPage * itemsPerPage;
+                final int endIndex = (currentPage + 1) * itemsPerPage;
+
+                final List<PVModel> currentPageData = invList.sublist(
+                  startIndex,
+                  endIndex < invList.length ? endIndex : invList.length,
+                );
+
+                return Column(
+                  children: [
+                    DataTable(
+                      columns: const [
+                        DataColumn(label: Text('Attribute')),
+                        DataColumn(label: Text('Value (Inverter 1)')),
+                        DataColumn(label: Text('Value (Inverter 2)')),
+                        DataColumn(label: Text('Value (Inverter 3)')),
+                      ],
+                      rows: [
+                        for (final pvModel in currentPageData)
+                          ...pvModel.toJson().entries.map((entry) {
+                            final attribute = entry.key;
+                            final inv1Value = entry.value.toString();
+                            final inv2Value = pvModel.toJson()[entry.key].toString();
+                            final inv3Value = pvModel.toJson()[entry.key].toString();
+                            return DataRow(
+                              cells: [
+                                DataCell(Text(attribute)),
+                                DataCell(Text(inv1Value)),
+                                DataCell(Text(inv2Value)),
+                                DataCell(Text(inv3Value)),
+                              ],
+                            );
+                          }),
+                      ],
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: _pvController.previousPage,
+                        ),
+                        Text('Page ${currentPage + 1}'),
+                        IconButton(
+                          icon: const Icon(Icons.arrow_forward),
+                          onPressed: _pvController.nextPage,
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
             ),
+          ),
         ),
-        );
+      ),
+    );
   }
 }
+
+
